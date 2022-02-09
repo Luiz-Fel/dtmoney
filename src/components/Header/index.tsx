c0d3103c5ccc973756f3a9f793f034bd00bf6827
Luiz Felipe <lfsprj@gmail.com>
2022-02-08 21:38:15 -0300

    Header name solved.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import  logoImg from "../../assets/Logo.svg";
+import { Container, Content } from "./styles";
+
+interface PropsTransactionModal {
+    openModalFunction: () => void,
+}
+
+
+export default function Header({openModalFunction} :PropsTransactionModal) {
+    return (
+    <Container>
+        <Content>
+            <a href="/">
+                <img src={logoImg} alt="dt money" />
+            </a>
+            <button type="button" onClick={openModalFunction}>Nova transação</button>
+        </Content>
+    </Container>
+    )}
